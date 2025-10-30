bf0473964bd8ba853db730d298251174e8451318
gnix22 <gjnix22@gmail.com>
2025-10-30 13:10:35 -0400

    got retro wezterm tab to what I like (thanks james for dot file reference)

--- modules/configurations/wezterm.nix
+++ modules/configurations/wezterm.nix
@@ -5,14 +5,37 @@
     programs.wezterm = {
       enable = true;
       enableBashIntegration = true;
+      /* lua */
       extraConfig = ''
         local wezterm = require("wezterm")
-        return {
-          font = wezterm.font("GohuFont"),
-          color_scheme = "Belafonte Night (Gogh)",
-          font_size = 12,
-          default_prog = { "bash" },
+        local config = wezterm.config_builder()
+
+        -- font settings 
+        config.font = wezterm.font("GohuFont")
+        config.font_size = 12
+
+        -- colors 
+        config.color_scheme = "Belafonte Night (Gogh)"
+
+        -- tab bar settings
+        config.use_fancy_tab_bar = false
+        config.hide_tab_bar_if_only_one_tab = true
+        config.colors = {
+          tab_bar = {background = '#000000',
+            active_tab = {
+              bg_color = '#1C1C1C',
+              fg_color = '#ffffff',
+            },
+            inactive_tab = {
+              bg_color = '#000000',
+              fg_color = '#ffffff',
+            },
+          },
+
         }
+        -- gets program to load .bashrc
+        default_prog = {"bash"}
+        return config
       '';
     };
   };
